58a2cfdcd8be5c38d61759a11d72630038f2a217
Poliester2005 <107966984+Poliester2005@users.noreply.github.com>
2026-06-26 01:39:53 -0300

    Doc open rich text

--- resources/document_data.gd
+++ resources/document_data.gd
@@ -3,7 +3,7 @@ class_name DocumentData
 extends Resource
 
 @export var id: int
-@export var texture: Texture2D
-@export var collision_radius: float = 82.0
 @export var document_name: String = ""
-@export var document_text: String = "" # Rich text Label
+@export var texture: Texture2D
+@export var collision_radius: float = 20.0
+@export_multiline var conteudo: String = ""
